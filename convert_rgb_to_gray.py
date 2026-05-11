2c0b55ce09a29adb86f69caa08f807715f131e0c
mdshiblinoman <mdshiblisharkar@gmail.com>
2026-05-11 19:14:14 +0600

    upload convert image rgb to gray

---
+++ convert_rgb_to_gray.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('Rose2.png')
+
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imwrite('Rose2_gray.png', img_gray)
+
+# print the image properties
+print(img_gray.shape)
+print(img_gray.size)
+print(type(img_gray))
+
+# show the image
+cv2.imshow('image', img_gray)
+cv2.waitKey(0)
