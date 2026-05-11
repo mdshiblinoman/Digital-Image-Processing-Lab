79a731dd64ad921b75e3cfe9d8a8d5e2a7c5f42d
mdshiblinoman <mdshiblisharkar@gmail.com>
2026-05-11 19:09:01 +0600

    upload image procssing

---
+++ read_and_show_image.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('Rose1.png')
+
+# print the image properties
+# print(img)
+print(img.shape)
+print(img.size)
+print(type(img))
+
+# show the image
+cv2.imshow('image', img)
+cv2.waitKey(0)
